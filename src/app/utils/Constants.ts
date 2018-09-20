a442ffe5cc770608fa4e9578cd6d2001cfc7497c
david <davhoj@gmail.com>
2018-09-20 12:22:11 -0300

    prueba

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -1,5 +1,5 @@
 export class Constants{
-  static BASE_URL:string = "http://ec2-34-202-237-163.compute-1.amazonaws.com:8888/";
+  static BASE_URL:string = "http://ec2-52-90-154-102.compute-1.amazonaws.com:8888/";
   //static BASE_URL:string = "http://localhost:8080/";
 }
 ///loginController/isUsedId
