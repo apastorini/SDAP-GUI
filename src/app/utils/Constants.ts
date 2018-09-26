571351c2d5086cc2f77fbcf86baedb038bb6667e
david <davhoj@gmail.com>
2018-09-25 23:34:12 -0300

    cambio de URL IP AMAZON

--- src/app/utils/Constants.ts
+++ src/app/utils/Constants.ts
@@ -1,5 +1,5 @@
 export class Constants{
-  static BASE_URL:string = "http://ec2-52-90-154-102.compute-1.amazonaws.com:8888/";
+  static BASE_URL:string = "http://ec2-18-215-158-201.compute-1.amazonaws.com:8888/";
   //static BASE_URL:string = "http://localhost:8080/";
 }
 
